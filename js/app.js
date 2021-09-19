255b1cb3dabc483c0157c456aba9ba7271bb887c
yaakov04 <dbrionesmtz@yahoo.com>
2021-09-19 08:22:13 -0500

    Agregue funcion para calcular la moda

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const lista = [100, 500, 70, 200, 800,4]
+const lista = [100, 500, 70, 200, 800, 4, 500, 500, 100, 70, 70, 70, 70, 70, 70]
     //calcular promedio
 const calcularPromedio = lista => {
     let sum = lista.reduce((suma, elemento) => suma + elemento);
@@ -7,7 +7,7 @@ const calcularPromedio = lista => {
 
 //calcular mediana
 const calcularMediana = (lista) => {
-	lista=lista.sort((a,b)=>a-b);
+    lista = lista.sort((a, b) => a - b);
     const mitadLista = lista.length / 2;
     if (lista.length % 2 === 0) {
         const promedio = calcularPromedio([lista[mitadLista], lista[mitadLista - 1]])
@@ -20,3 +20,18 @@ const calcularMediana = (lista) => {
 
 //calcular moda
 
+const calcularModa = (lista) => {
+    const listaCount = {};
+
+    lista.map(
+        (e) => {
+            (listaCount[e]) ? listaCount[e] += 1: listaCount[e] = 1
+        }
+    );
+
+    const arrayCount = Object.entries(listaCount).sort(
+        (e, i) => e[1] - i[1]
+    );
+    const moda = arrayCount[arrayCount.length - 1][0];
+    return moda;
+}
